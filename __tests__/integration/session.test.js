488a1740551cd72457ee179d22a19fe5244c1695
Yitshhaq Fukushima <ymotse@gmail.com>
2020-09-02 19:51:42 -0300

    Tests with private routes

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -16,8 +16,6 @@ describe('Authentication', () => {
             password: '123123'
         })
         
-        console.log(user)
-        
         const response = await request(app)
         .post('/sessions')
         .send({
@@ -57,4 +55,32 @@ describe('Authentication', () => {
             
         expect(response.body).toHaveProperty('token')
     })
+    
+    it('should be able to access private routes when authenticated', async () => {
+        const user = await factory.create('User', {
+            password: '123123'
+        })
+        
+        const response = await request(app)
+            .get('/dashboard')
+            .set("Authorization", `Bearer ${user.generateToken()}`)
+            
+        expect(response.status).toBe(200)
+    })
+    
+    it('should not be able to access private routes without jwt token', async () => {
+        const response = await request(app)
+            .get('/dashboard')
+    
+        expect(response.status).toBe(401)
+    })
+    
+    it('should not be able to access private routes with invalid jwt token', async () => {
+        const response = await request(app)
+          .get('/dashboard')
+          .set("Authorization", `Bearer 123123`)
+    
+        expect(response.status).toBe(401)
+    })
+    
 })
